5278d761a9efe86aebd80dbf4c73fc509e6210d7
Zebych <81613417+Zebych@users.noreply.github.com>
2021-12-08 15:07:11 +0300

    img

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const photo1 = require('./img/680395566_w640_h640_kruzhka-s-prikolom.jpg');
+/*const photo1 = require('./img/680395566_w640_h640_kruzhka-s-prikolom.jpg');
 const photo3 = require('./img/6-1000x1000.jpg');
 const photo2 = require('./img/6064641689.jpg');
 const photo4 = require('./img/kruzhka_sgushchenka_img.webp');
 const photo5 = require('./img/people_2_mug_chameleon_front_whitered_500.jpg');
 const photo7 = require('./img/pic1white.jpg');
-const photo6 = require('./img/product_57508_0_0_0.jpg');
+const photo6 = require('./img/product_57508_0_0_0.jpg');*/
 
 const express = require('express');
 
@@ -14,15 +14,24 @@ const app = express();
 app.use(cors({"origin": "*"}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + './img/680395566_w640_h640_kruzhka-s-prikolom.jpg'));
 
+const photo3 = '<img src="./img/680395566_w640_h640_kruzhka-s-prikolom.jpg"/>';
 const arrData = [
-    {name: 'mug1', photo: photo1, id: 1, price: 50, toPurchase: 1, inStock: 10},
-    {name: 'mug3', photo: photo3, id: 3, price: 90, toPurchase: 1, inStock: 10},
-    {name: 'mug2', photo: photo2, id: 2, price: 70, toPurchase: 1, inStock: 10},
-    {name: 'mug4', photo: photo4, id: 4, price: 100, toPurchase: 1, inStock: 10},
-    {name: 'mug5', photo: photo5, id: 5, price: 110, toPurchase: 1, inStock: 10},
-    {name: 'mug6', photo: photo6, id: 6, price: 120, toPurchase: 1, inStock: 10},
-    {name: 'mug7', photo: photo7, id: 7, price: 130, toPurchase: 1, inStock: 10},
+    {
+        name: 'mug1',
+        photo: photo3,
+        id: 1,
+        price: 50,
+        toPurchase: 1,
+        inStock: 10
+    },
+    /*    {name: 'mug3', photo: photo3, id: 3, price: 90, toPurchase: 1, inStock: 10},
+        {name: 'mug2', photo: photo2, id: 2, price: 70, toPurchase: 1, inStock: 10},
+        {name: 'mug4', photo: photo4, id: 4, price: 100, toPurchase: 1, inStock: 10},
+        {name: 'mug5', photo: photo5, id: 5, price: 110, toPurchase: 1, inStock: 10},
+        {name: 'mug6', photo: photo6, id: 6, price: 120, toPurchase: 1, inStock: 10},
+        {name: 'mug7', photo: photo7, id: 7, price: 130, toPurchase: 1, inStock: 10},*/
 ];
 const purchasesData = [];
 
