22d99ba779eb1b5583aa6f4ffa37cf94b721ccb4
Zebych <81613417+Zebych@users.noreply.github.com>
2021-12-14 18:27:44 +0300

    local database

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-
-//Для вывода в консоль ошибок promise
-process.on('unhandledRejection',(reason,p)=>{
-    console.log(reason,p);
-});
-
-const cors = require("cors");
-
-const mongoose = require("mongoose");
 const {addUserData, allGoods} = require("./repository");
+const cors = require("cors");
+const bodyParser = require("body-parser")
+// const goods = require("./controller")
 
-mongoose.connect('mongodb://localhost/test-shop', {useMewUrlParser: true});
+// MongoDB
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 // mongoose.connect(`mongodb:${process.env.PORT}`, {useMewUrlParser: true});
 
 const db = mongoose.connection;
@@ -21,73 +20,75 @@ db.once('open', function () {
 
 
 const app = express();
-app.use(cors({"origin": "*"}));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-const arrData = [
-    {
-        name: 'mug1',
-        photo: '',
-        id: 1,
-        price: 50,
-        toPurchase: 1,
-        inStock: 10
-    },
-    {
-        name: 'mug3',
-        photo: '',
-        id: 3,
-        price: 90,
-        toPurchase: 1,
-        inStock: 10
-    },
-    {
-        name: 'mug2',
-        photo: '',
-        id: 2,
-        price: 70,
-        toPurchase: 1,
-        inStock: 10
-    },
-    {
-        name: 'mug4',
-        photo: '',
-        id: 4,
-        price: 100,
-        toPurchase: 1,
-        inStock: 10
-    },
-    {
-        name: 'mug5',
-        photo: '',
-        id: 5,
-        price: 110,
-        toPurchase: 1,
-        inStock: 10
-    },
-    {
-        name: 'mug6',
-        photo: '',
-        id: 6,
-        price: 120,
-        toPurchase: 1,
-        inStock: 10
-    },
-    {
-        name: 'mug7',
-        photo: '',
-        id: 7,
-        price: 130,
-        toPurchase: 1,
-        inStock: 10
-    },
-];
+// app.use("/test--shop",goods)
+
+// const arrData = [
+//     {
+//         name: 'mug1',
+//         photo: '',
+//         id: 1,
+//         price: 50,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+//     {
+//         name: 'mug3',
+//         photo: '',
+//         id: 3,
+//         price: 90,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+//     {
+//         name: 'mug2',
+//         photo: '',
+//         id: 2,
+//         price: 70,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+//     {
+//         name: 'mug4',
+//         photo: '',
+//         id: 4,
+//         price: 100,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+//     {
+//         name: 'mug5',
+//         photo: '',
+//         id: 5,
+//         price: 110,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+//     {
+//         name: 'mug6',
+//         photo: '',
+//         id: 6,
+//         price: 120,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+//     {
+//         name: 'mug7',
+//         photo: '',
+//         id: 7,
+//         price: 130,
+//         toPurchase: 1,
+//         inStock: 10
+//     },
+// ];
 const purchasesData = [];
 
 app.get('/test--shop-with-goods', async (req, res) => {
-    await allGoods()
-    res.send(arrData);
+    let goods = await allGoods()
+    res.send(goods);
 });
 app.post('/cart', async (req, res) => {
     /*    let userData = req.body.values;
